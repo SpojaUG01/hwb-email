723d6bc02074357572b8213095e144b011e4d55a
SPOJAUG01) <spojaug01@gmail.com>
2024-07-20 19:02:46 +0200

    Fix deployment issue by replacing readFileStr with Deno.readTextFile

--- main.ts
+++ main.ts
@@ -1,64 +1,37 @@
 import { serve } from "https://deno.land/std@0.135.0/http/server.ts";
-import { join, extname } from "https://deno.land/std@0.135.0/path/mod.ts";
-import { readFileStr } from "https://deno.land/std@0.135.0/fs/mod.ts";
+import { extname } from "https://deno.land/std@0.135.0/path/mod.ts";
 
-const PORT = 8000;
+const server = serve({ port: 8000 });
 
-const server = serve({ port: PORT });
-console.log(`Server running on http://localhost:${PORT}`);
+console.log("HTTP webserver running. Access it at: http://localhost:8000/");
 
-for await (const request of server) {
+for await (const req of server) {
+  let filePath = `./views${req.url}`;
+  if (req.url === "/") {
+    filePath = "./views/login.html";
+  }
+
   try {
-    let filePath = new URL(request.url, `http://localhost:${PORT}`).pathname;
+    const fileContents = await Deno.readTextFile(filePath);
+    let contentType = "text/html";
 
-    // Default to serving `login.html` on root path
-    if (filePath === "/") {
-      filePath = "/views/login.html";
-    } else {
-      // Serve files from the `public` directory if requested
-      if (filePath.startsWith("/public/")) {
-        filePath = `.${filePath}`;
-      } else {
-        filePath = `./views${filePath}`;
-      }
-    }
-
-    const fullPath = join(Deno.cwd(), filePath);
-    const fileExtension = extname(fullPath);
-    let contentType = "text/plain";
-
-    switch (fileExtension) {
-      case ".html":
-        contentType = "text/html";
-        break;
+    switch (extname(filePath)) {
       case ".css":
         contentType = "text/css";
         break;
       case ".js":
         contentType = "application/javascript";
         break;
-      // Add more cases as needed
     }
 
-    try {
-      const fileContent = await readFileStr(fullPath);
-      request.respond({
-        status: 200,
-        body: fileContent,
-        headers: new Headers({
-          "Content-Type": contentType,
-        }),
-      });
-    } catch (e) {
-      request.respond({
-        status: 404,
-        body: "404 Not Found",
-      });
-    }
-  } catch (e) {
-    request.respond({
-      status: 500,
-      body: "500 Internal Server Error",
+    req.respond({
+      status: 200,
+      body: fileContents,
+      headers: new Headers({
+        "Content-Type": contentType,
+      }),
     });
+  } catch {
+    req.respond({ status: 404, body: "File not found" });
   }
 }
